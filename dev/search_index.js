5c7765dbaa5f7ca3bde98ec26a2ff5900bbeec5a
Documenter.jl <documenter@juliadocs.github.io>
2025-03-16 12:18:06 +0000

    build based on 47e9d93

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PiecewiseVanillaModel","category":"page"},{"location":"#PiecewiseVanillaModel","page":"Home","title":"PiecewiseVanillaModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PiecewiseVanillaModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PiecewiseVanillaModel]","category":"page"}]
+}
