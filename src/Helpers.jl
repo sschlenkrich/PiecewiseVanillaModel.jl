021343361b5b3c0dd342a316c50993726a25b683
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2025-09-11 08:20:43 +0200

    Refactor black and bachelier formula

--- src/Helpers.jl
+++ src/Helpers.jl
@@ -24,17 +24,23 @@ Argument nu represents the standard deviation of the forward price. For
 annualised lognormal volatility σ, we have ``ν = σ √T``. Finally, `call_put`
 is the call (+1) or put (-1) option flag.
 
+Note that we allow negative standard deviation ν. This allows modelling negative
+skew for shifted-lognormal models.
+
+Note that we do not test for vanishing volatility. Zero volatility may cause
+floating point errors.
+
 We allow broadcasting for arguments.
 """
 function black_price(strike, forward, nu, call_put)
-    intrinsic_value = max.(call_put * (forward .- strike), 0.0)
     d1 = _d_1(strike, forward, nu)
     d2 = d1 .- nu
     option_value = call_put * (
         forward .* cdf.(Normal(), call_put .* d1) .-
         strike  .* cdf.(Normal(), call_put .* d2)
         )
-    return (nu .> 0.0) .* option_value .+ (nu .<= 0.0) .* intrinsic_value
+    intrinsic_value = max.(call_put * (forward .- strike), 0.0)
+    return max.(option_value, intrinsic_value)
 end
 
 """
@@ -56,12 +62,16 @@ pricing measure.
 Argument `nu` represents the standard deviation of the forward price. For
 annualised normal volatility σ, we have `ν = σ √T`. Finally, `call_put`
 is the call (+1) or put (-1) option flag.
+
+Note that we do not test for vanishing volatility. Zero volatility may cause
+floating point errors.
 """
 function bachelier_price(strike, forward, nu, call_put)
-    intrinsic_value = max.(call_put .* (forward .- strike), 0.0)
-    h = call_put .* (forward .- strike) ./ nu
-    option_value = nu .* (h .* cdf.(Normal(), h) .+ pdf.(Normal(), h))
-    return (nu .> 0.0) .* option_value .+ (nu .<= 0.0) .* intrinsic_value
+    u = call_put .* (forward .- strike)
+    h = u ./ nu
+    option_value = u .* cdf.(Normal(), h) .+ nu .* pdf.(Normal(), h)
+    intrinsic_value = max.(u, 0.0)
+    return max.(option_value, intrinsic_value)
 end
 
 """
